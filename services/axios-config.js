c8b4ead1397cab64cf5c845bebcbbef032ac4021
Dan Chuzhov <dan.chuzhov.htc@gmail.com>
2023-09-15 08:43:16 +0300

    add SSL error handler

--- services/axios-config.js
+++ services/axios-config.js
@@ -1,9 +1,15 @@
 const axios = require('axios');
+const https = require('https');
 
-// Create and configure your Axios instance
+// Create an agent with custom configuration to handle SSL certificate errors
+const agent = new https.Agent({
+  rejectUnauthorized: false,
+});
+
+// Create and configure your Axios instance with the custom agent
 const en_api = axios.create({
   baseURL: 'https://enext.ua',
-  // other configuration options
+  httpsAgent: agent, // Add this line to use the custom agent
 });
 
 // Export the configured Axios instance
